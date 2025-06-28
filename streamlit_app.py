ecc8f92330307dfcaf31478866434e0928d29328
Nithin7king <147793014+Nithin7king@users.noreply.github.com>
2025-06-28 11:16:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 
-st.title("🦋 Welcome to TrivAI!")
-st.subheader("A blend of emotions and logic, built with love and purpose by two passionate minds.")
+st.title("🦋 Welcome to Python Guru!")
+st.subheader("language learning model to any programming language.")
 st.text_input("Ask something:")
 st.markdown("📌 For help and inspiration, visit [docs.streamlit.io](https://docs.streamlit.io/)")
